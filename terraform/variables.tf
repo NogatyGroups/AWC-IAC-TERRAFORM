b2e0f6e6c1285c0052b0b89e2d8f12835f61e8ec
Bamba Houlouwolo-Pierre <gbigbi21041989@gmail.com>
2026-01-06 11:09:13 +0100

    Add Peering VPC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,9 +35,35 @@ variable "tf-bucket-source-s3crossreplicas" {
   
 }
 
+
 variable "tf-bucket-destination-s3crossreplicas" {
   type = string
   default = "nogaty-cross-replicas"
   
 }
 
+variable "nogaty-us-east-vpc" {
+  type = string
+  default = "nogaty-us-east-vpc"
+}
+
+variable "nogaty-us-west-vpc" {
+  type = string
+  default = "nogaty-us-west-vpc"
+}
+
+variable "peer-region" {
+  type = string
+  default = "us-west-1"
+  
+}
+
+variable "peer-owner-id" {
+  type = string
+  default = "712487951696"
+}
+
+variable "peering-vpc-name" {
+  type = string
+  default = "Nogaty-Peering-VPC"
+}
---
+++ terraform/vpc_peering.tf
@@ -0,0 +1,38 @@
+##########################################################################################################
+# VPC PEERING
+##########################################################################################################
+## Create VPC A 
+resource "aws_vpc" "nogaty-us-east-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = var.nogaty-us-east-vpc
+  }
+}
+
+
+## Create VPC B
+resource "aws_vpc" "nogaty-us-west-vpc" {
+  cidr_block = "192.168.0/16"
+  tags = {
+    Name = var.nogaty-us-west-vpc
+  }
+}
+
+
+### Create Peering between VPC-A and VPC-B
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  peer_owner_id = var.peer-owner-id
+  peer_region = var.peer-region
+  peer_vpc_id = aws_vpc.nogaty-us-west-vpc.id 
+  vpc_id = aws_vpc.nogaty-us-east-vpc.id
+  auto_accept = false
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+    }
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+  tags = {
+    Name = var.peering-vpc-name
+  }
+}
