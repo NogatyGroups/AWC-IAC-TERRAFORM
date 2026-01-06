412139c7c10eda1f33419831d071978946b05db3
Bamba Houlouwolo-Pierre <gbigbi21041989@gmail.com>
2026-01-06 21:11:26 +0100

    Add lambda

--- terraform/lambda-node-js.tf
+++ terraform/lambda-node-js.tf
@@ -1,5 +1,8 @@
+###################################################################################
+# Zip a function
+###################################################################################
 ## Zip the function to be run at function App
-data "archive_file" "init" {
+data "archive_file" "lambda_zip" {
     type = "zip"
     source_file = "${path.module}/Projet-nodejs/hello.js"
     output_path = "${path.module}/hello.zip"
@@ -18,10 +21,10 @@ resource "aws_s3_bucket" "nogaty-bucket-lambda" {
 
 ### Upload zip file to S3 bucket
 resource "aws_s3_object" "object" {
-  bucket = "${var.lambda-bucket}-01"
+  bucket = aws_s3_bucket.lambda_bucket.bucket
   key    = "hello.zip"
-  source = "${path.module}/hello.zip"
-  etag   = filemd5("${path.module}/hello.zip")
+  source = data.archive_file.lambda_zip.output_path
+  etag   = data.archive_file.lambda_zip.output_base64sha256
 }
 
 ###################################################################################
@@ -72,11 +75,11 @@ resource "aws_iam_role_policy_attachment" "lambda-role-policy-attachment" {
 # Create Lambda function
 ###################################################################################
 resource "aws_lambda_function" "lambda-nodejs" {
-  filename      = data.archive_file.init.output_path
+  filename      = data.archive_file.lambda_zip.output_path
   function_name = var.lambda-nodejs-function
   role          = aws_iam_role.lambda-iam-role.arn
   handler       = "index.handler"
-  code_sha256   = data.archive_file.init.output_base64sha256
+  code_sha256   = data.archive_file.lambda_zip.output_base64sha256
 
   runtime = "nodejs20.x"
 
@@ -92,3 +95,7 @@ resource "aws_lambda_function" "lambda-nodejs" {
     Application = "lambda-nodejs-function"
   }
 }
+
+######################################################################################
+# A revoir: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
+#######################################################################################
