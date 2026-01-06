5d56910507a0e28479fca1ea8dcd894e4afd2c9c
Bamba Houlouwolo-Pierre <gbigbi21041989@gmail.com>
2026-01-06 13:23:09 +0100

    Change region us-west to eu-central

---
+++ terraform/ec2-instances.tf
@@ -0,0 +1,50 @@
+data "aws_ami" "ubuntu-a" {
+    provider = aws.vpc-a
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+data "aws_ami" "ubuntu-b" {
+    provider = aws.vpc-b
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "nogaty-ec2-vpc-b" {
+    provider = aws.vpc-a
+    ami           = data.aws_ami.ubuntu-a.id
+    instance_type = "t3.micro"
+
+    tags = {
+      Name = "nogaty-ec2-vpc-a"
+    }
+}
+
+resource "aws_instance" "nogaty-ec2-vpc-b" {
+    provider = aws.vpc-a
+    ami           = data.aws_ami.ubuntu-b.id
+    instance_type = "t3.micro"
+
+    tags = {
+      Name = "nogaty-ec2-vpc-b"
+    }
+}
